53013d203b4f941ca195257e35ca3aaa430ef2af
nylvenic <85847342+nylvenic@users.noreply.github.com>
2022-12-25 19:19:18 +0000

    Add form validation and an alert

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,9 @@ const calculateBtn = document.querySelector('#calculateBtn');
 const monthlyPayment = document.querySelector('#monthlyPayment');
 const totalPayment = document.querySelector('#totalPayment');
 const totalInterest = document.querySelector('#totalInterest');
+const alertWindow = document.querySelector('#alertWindow');
+const alertContent = document.querySelector('#alertContent');
+let alertInterval;
 
 calculateBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,8 +13,30 @@ calculateBtn.addEventListener('click', (e) => {
     const interestRate = document.querySelector('#interestRate').value;
     const years = document.querySelector('#years').value;
 
+    if(!loanAmount) {
+        createAlert('Please enter a loan amount!');
+        return;
+    }
+    else if(!interestRate) {
+        createAlert('Please enter an interest rate!');
+        return;
+    }
+    else if(!years) {
+        createAlert("Please enter how long you'll be paying it off!");
+        return;
+    }
+
     calculate(loanAmount, interestRate, years);
 });
+
+function createAlert(msg) {
+    clearInterval(alertInterval);
+    alertContent.textContent = msg;
+    alertWindow.classList.remove('hidden');
+    alertInterval = setInterval(() => {
+        alertWindow.classList.add('hidden');
+    }, 2500);
+}
 
 function calculate(loanAmount, interestRate, years) {
     loanAmount = parseFloat(loanAmount);
@@ -22,7 +47,7 @@ function calculate(loanAmount, interestRate, years) {
     const totalPaymentResult = loanAmount + interestResult;
     const monthlyPaymentResult = totalPaymentResult / (years * 12);
 
-    monthlyPayment.value = monthlyPaymentResult;
-    totalPayment.value = totalPaymentResult;
-    totalInterest.value = interestResult;
+    monthlyPayment.value = monthlyPaymentResult.toFixed(2);
+    totalPayment.value = totalPaymentResult.toFixed(2);
+    totalInterest.value = interestResult.toFixed(2);
 }
